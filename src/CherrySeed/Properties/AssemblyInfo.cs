ef18a24a0e63aee83680979de134254ee6a332b0
Michael Altmann <michaelaltmann@outlook.com>
2016-08-06 22:33:13 +0200

    Change description

--- src/CherrySeed/Properties/AssemblyInfo.cs
+++ src/CherrySeed/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CherrySeed")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CherrySeed helps you to define test data and seed it into repositories.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Michael Altmann")]
 [assembly: AssemblyProduct("CherrySeed")]

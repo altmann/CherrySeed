fd761a73e83f0f8f6e032744ac2bdb5716b976d1
Michael Altmann <michaelaltmann@outlook.com>
2016-08-11 15:51:30 +0200

    Add some type transformations

---
+++ src/CherrySeed/TypeTransformations/DecimalTransformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CherrySeed.TypeTransformations
+{
+    public class DecimalTransformation : ITypeTransformation
+    {
+        public object Transform(Type type, string str)
+        {
+            return decimal.Parse(str);
+        }
+    }
+}
---
+++ src/CherrySeed/TypeTransformations/DoubleTransformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CherrySeed.TypeTransformations
+{
+    public class DoubleTransformation : ITypeTransformation
+    {
+        public object Transform(Type type, string str)
+        {
+            return double.Parse(str);
+        }
+    }
+}

0695943df4d9fc3790201fb628dcd42a92999d73
Michael Altmann <michaelaltmann@outlook.com>
2016-08-13 15:32:31 +0200

    Add tests for custom empty string marker

---
+++ src/CherrySeed.Test/UnitTests/ObjectWithCustomEmptyStringTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using CherrySeed.Configuration;
+using CherrySeed.EntityDataProvider;
+using CherrySeed.Repositories;
+using CherrySeed.Test.Asserts;
+using CherrySeed.Test.Mocks;
+using CherrySeed.Test.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CherrySeed.Test.UnitTests
+{
+    [TestClass]
+    public class ObjectWithCustomEmptyStringTests
+    {
+        [TestMethod]
+        public void ObjectWithCustomEmptyString()
+        {
+            var entityData = new List<EntityData>
+            {
+                new EntityData
+                {
+                    EntityName = "CherrySeed.Test.Models.Sub",
+                    Objects = new List<Dictionary<string, string>>
+                    {
+                        new Dictionary<string, string>
+                        {
+                            { "Id", "1" },
+                            { "MyString", "%" },
+                        },
+                        new Dictionary<string, string>
+                        {
+                            { "Id", "2" },
+                            { "MyString", "MyString 2" },
+                        }
+                    }
+                },
+            };
+
+            var assertRepository = new AssertRepository((obj, count) =>
+            {
+                AssertHelper.AssertIf(typeof(Sub), 0, count, obj, () =>
+                {
+                    AssertSub.AssertProperties(obj, "");
+                });
+
+                AssertHelper.AssertIf(typeof(Sub), 1, count, obj, () =>
+                {
+                    AssertSub.AssertProperties(obj, "MyString 2");
+                });
+            }, type =>
+            {
+                
+            });
+            
+            InitAndExecute(entityData, assertRepository, cfg =>
+            {
+                cfg.WithEmptyStringMarker("%");
+
+                cfg.ForEntity<Sub>()
+                    .WithIntegerIdGenerationViaCode();
+            });
+        }
+
+        private void InitAndExecute(List<EntityData> data, IRepository repository, 
+            Action<ISeederConfigurationBuilder> entitySettings)
+        {
+            var config = new SeederConfiguration(cfg =>
+            {
+                cfg.WithDataProvider(new DictionaryDataProvider(data));
+                cfg.WithDefaultRepository(repository);
+
+                entitySettings(cfg);
+            });
+
+            var cherrySeeder = config.CreateSeeder();
+            cherrySeeder.Seed();
+        }
+    }
+}
